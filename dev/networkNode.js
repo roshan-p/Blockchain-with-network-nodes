b756e540f28929347dcd875017b0545cefffe877
Roshan Pandey <roshan007191@gmail.com>
2019-02-04 11:54:48 +0700

    Update new mine block function

--- dev/networkNode.js
+++ dev/networkNode.js
@@ -21,7 +21,7 @@ app.get('/blockchain', function (req, res) {
 app.post('/transaction', function (req, res) {
     const newTransacion = req.body;
     const blockIndex = bitcoin.addTransactionToPendingTransactions(newTransacion);
-    res.json({note:`Transaction will be added in block ${blockIndex}.`})
+    res.json({ note: `Transaction will be added in block ${blockIndex}.` })
     // const blockIndex = bitcoin.createNewTransaction(req.body.amount, req.body.sender, req.body.recipient);
     // res.json({ note: `Transaction will be added in block ${blockIndex}.` });
 });
@@ -35,39 +35,84 @@ app.post('/transaction/broadcast', function (req, res) {
     bitcoin.networkNodes.forEach(networkNodeUrl => {
         const requestOptions = {
             uri: networkNodeUrl + '/transaction',
-            body:newTransacion,
+            body: newTransacion,
             method: 'POST',
             json: true
         };
         requestPromises.push(rp(requestOptions));
     });
     Promise.all(requestPromises)
-    .then(data=>{
-        res.json({note:'Transaction created and broadcast successfully.'})
-    });
+        .then(data => {
+            res.json({ note: 'Transaction created and broadcast successfully.' })
+        });
 });
 
 app.get('/mine', function (req, res) {
     const lastBlock = bitcoin.getLastBlock();
-    const previcousBlockHash = lastBlock['hash'];
-    const currentblockData = {
-        transaction: bitcoin.pendingTransactions,
+    const previousBlockHash = lastBlock['hash'];
+    const currentBlockData = {
+        transactions: bitcoin.pendingTransactions,
         index: lastBlock['index'] + 1
-    }
+    };
+    const nonce = bitcoin.proofOfWork(previousBlockHash, currentBlockData);
+    const blockHash = bitcoin.hashBlock(previousBlockHash, currentBlockData, nonce);
+    const newBlock = bitcoin.createNewBlock(nonce, previousBlockHash, blockHash);
 
-    const nonce = bitcoin.proofOfWork(previcousBlockHash, currentblockData);
-    const blockHash = bitcoin.hashBlock(previcousBlockHash, currentblockData, nonce);
+    const requestPromises = [];
+    bitcoin.networkNodes.forEach(networkNodeUrl => {
+        const requestOptions = {
+            uri: networkNodeUrl + '/receive-new-block',
+            method: 'POST',
+            body: { newBlock: newBlock },
+            json: true
+        };
 
-    bitcoin.createNewTransaction(12.5, "00", nodeAddress)
+        requestPromises.push(rp(requestOptions));
+    });
 
-    const newBlock = bitcoin.createNewBlock(nonce, previcousBlockHash, blockHash);
+    Promise.all(requestPromises)
+        .then(data => {
+            const requestOptions = {
+                uri: bitcoin.currentNodeUrl + '/transaction/broadcast',
+                method: 'POST',
+                body: {
+                    amount: 12.5,
+                    sender: "00",
+                    recipient: nodeAddress
+                },
+                json: true
+            };
 
-    res.json({
-        note: 'New block successfully mined',
-        block: newBlock,
-    })
+            return rp(requestOptions);
+        })
+        .then(data => {
+            res.json({
+                note: "New block mined & broadcast successfully",
+                block: newBlock
+            });
+        });
 });
 
+app.post('/receive-new-block', function(req, res) {
+	const newBlock = req.body.newBlock;
+	const lastBlock = bitcoin.getLastBlock();
+	const correctHash = lastBlock.hash === newBlock.previousBlockHash; 
+	const correctIndex = lastBlock['index'] + 1 === newBlock['index'];
+
+	if (correctHash && correctIndex) {
+		bitcoin.chain.push(newBlock);
+		bitcoin.pendingTransactions = [];
+		res.json({
+			note: 'New block received and accepted.',
+			newBlock: newBlock
+		});
+	} else {
+		res.json({
+			note: 'New block rejected.',
+			newBlock: newBlock
+		});
+	}
+});
 // register a node and broadcast it to entire network
 app.post('/register-add-broadcast-node', function (req, res) {
     const newNodeUrl = req.body.newNodeUrl;
